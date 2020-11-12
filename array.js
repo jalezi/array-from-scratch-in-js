1f0748a9136556c774567825c5a397d83c19008d
Mark Tellez <mark.tellez@icloud.com>
2020-11-12 13:49:05 -0600

    added array, push, pop

---
+++ array.js
@@ -0,0 +1,40 @@
+function Array2() {
+  this.data = {};
+  this.length = this.lastKey();
+}
+
+Array2.prototype.length = function () {
+  return this.lastKey() + 1;
+};
+
+Array2.prototype.lastKey = function () {
+  return Math.max(...Object.keys(this.data));
+};
+
+Array2.prototype.push = function (item) {
+  const key = (this.lastKey() < 0
+    ? 0
+    : this.lastKey() + 1
+  ).toString();
+  this.data = { ...this.data, [key]: item };
+  this.length = this.length();
+};
+
+Array2.prototype.pop = function () {
+  const lastItem = this.data[this.lastKey()];
+  let newObj = {};
+
+  for (let i = 0; i < Number(this.lastKey() + 1); i++) {
+    const newItem = this.data[i.toString()];
+    if (newItem !== lastItem) newObj[i] = newItem;
+  }
+  this.data = newObj;
+  this.length = this.length();
+  return lastItem;
+};
+
+a = new Array2();
+a.push('item 1');
+a.push('item 2');
+a.push('item 3');
+a.pop();
